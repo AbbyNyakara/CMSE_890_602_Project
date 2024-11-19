8e6a05277c43d90bea5ff342908363e5f442ae24
Abigael Nyakara <mogusuab@msu.edu>
2024-11-19 13:49:54 -0500

    SRC Code: Added information gathering, planning and prep and vulnerability analysis code

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
 
 """
-This program simulates a cybersecurity penetration testing process. 
-The limits of this is network logs,where the network is scanned, 
-the vulnerabilities detected, exploited and ends up with a pen-test report. 
+This proogram simulates a cybersecurity penetration testing process.
+
+This program emulates the key stages of a network penetration test:
+1. Preparation
+2. Network scanning / information gathering
+3. Vulnerability detection and analysis
+4. Exploitation phase
+5. Risk Assesment
+6. Final Report generation
+
+The simulation is limited to analyzing network logs and does not perform actual network operations.
+The reports generated in this process are not actual cases, but dummy examples. 
 """
 
-def pentest_planning():
-    """
-    Mimics the planning phase of a pentest process
 
-    This function 
-    """
+
+
---
+++ src/planning_and_preparation.py
@@ -0,0 +1,33 @@
+
+import datetime
+
+
+def planning_and_preparation():
+    """
+    Simulates the planning and preparation phase of a penetration test.
+
+    function gathers system information, defines the scope of the test,
+    and writes this information to a report file.
+
+    Returns:
+    report_file
+    """
+    # Define the details
+    system_info = "Sample System: Linux Server"
+    scope_allowed = "In-scope: Internal network (192.168.1.0/24)\n Out-of-scope: External facing services"
+
+    now = datetime.datetime.now()  # Create timestamp of report
+    report_content = f"""Penetration Test Planning report Generated at: {now}\n
+                    System Information:
+                    {system_info}
+
+                    Test Scope:
+                    {scope_allowed}
+                     """
+    # Create and write to a report file:
+    file = "pentest_planning_report.txt"
+
+    with open(file, 'a+', encoding='UTF-8') as report_file:
+        report_file.write(report_content)
+
+    return file

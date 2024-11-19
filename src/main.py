cef3bc30b6ac9b75a7d353826cf1903abb6c039b
Abigael Nyakara <mogusuab@msu.edu>
2024-11-19 12:29:11 -0500

    SRC CODE: Added the initial source code for pentest process

--- src/main.py
+++ src/main.py
@@ -1,44 +1,13 @@
-import random
 
-def information_gathering():
+"""
+This program simulates a cybersecurity penetration testing process. 
+The limits of this is network logs,where the network is scanned, 
+the vulnerabilities detected, exploited and ends up with a pen-test report. 
+"""
+
+def pentest_planning():
     """
-    Simulates the information gathering phase of a penetration test, primarily focusing on logs.
+    Mimics the planning phase of a pentest process
 
-    This function collects and logs simulated details about the target's infrastructure logs,
-    application logs,and syslogs. It returns these log details for further analysis.
-
-    Returns:
-        dict (log_details):
-            infrastructure_logs: Logs from infrastructure components (e.g., web server logs,
-            database server logs).application_logs: Logs from applications in use (e.g., 
-            application logs).
-            service_logs: Logs from services running on the infrastructure (e.g., HTTP logs,
-                        SSH logs).
+    This function 
     """
-    log_details = {
-        'infrastructure_logs': ['web server logs', 'database server logs', 
-                                'application server logs'],
-        'application_logs': ['web app logs', 'mobile app logs'],
-        'service_logs': ['HTTP logs', 'HTTPS logs', 'SSH logs']
-    }
-    return log_details
-
-
-def vulnerability_analysis(log_details):
-    """
-    Evaluates the aggregated log_details from the information_gathering() phase
-    and check whther there are vulnerabilities:
-
-    The function takes in the log_details as input and checks if any vulnerabilities are present.
-    If present, it proceeds to attempt an exploit by calling attempt_exploit().
-    Otherwise, it performs additional scans by calling additional_vulnerability_scans(). 
-    
-    Args:
-        log_details (dict): log details from information_gathering()
-
-    Returns:
-        vulnerability_report
-
-    """
-    
-    pass
